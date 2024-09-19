c0941dabc9a02d60a739b13f98d1b403610c7edf
JaigovindShenoy <jaigovindbshenoy@gmail.com>
2024-09-19 15:36:23 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 3000;  //port used to run the webapp
+// Below is the Updated code after creating the node_modules and dist file
+// Dist folder now contains the compressed index.html file with all necessary packages so we have only to run that file
 
+const express = require('express');
 const path = require('path');
 
-// the URL: localhost:3000/Website
+const app = express();
+const port = 3000;
 
-app.use('/Website', express.static(path.join(__dirname, 'code')));
+// Serve static files from the dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(function(req, res) {
-    res.status(400);
-    return res.send('404 Error: Resource not found');
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`App listening  on port ${port}`);
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
