e3a3c1c02566534166030ba9d9b55455a33204ff
JaigovindShenoy <jaigovindbshenoy@gmail.com>
2024-09-19 15:37:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,3 +17,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+// Below is the Code before building the DIST folder
+
+//const express = require('express');
+// const app = express();
+// const port = 3000;  //port used to run the webapp
+
+// const path = require('path');
+
+// // the URL: localhost:3000/Website
+
+// app.use('/Website', express.static(path.join(__dirname, 'code')));
+
+// app.use(function(req, res) {
+//     res.status(400);
+//     return res.send('404 Error: Resource not found');
+//   });
+
+// app.listen(port, () => {
+//   console.log(`App listening  on port ${port}`);
+// })
